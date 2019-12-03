ab93c55fa6668dc258ad22b5bbb746814d54b23c
Hafid Denguir <haf2com@gmail.com>
2019-12-03 13:52:39 +0100

    update db.js

--- config/db.js
+++ config/db.js
@@ -16,9 +16,9 @@ const connectDB = async () => {
   } catch (err) {
     console.log("========= MongoDB Disconnected ....======");
     console.log(err.message);
-    return res.status(400).json({
+    return {
       errors: [{ msg: err.message }]
-    });
+    };
     //process.exit(1);
   }
 };
